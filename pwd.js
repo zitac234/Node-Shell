de2576a819ac1965ab46e5734545373048bbab3a
Rafael Segarra <rafaelandres54@gmail.com>
2020-11-13 15:18:31 -0500

    ls added

---
+++ pwd.js
@@ -0,0 +1,12 @@
+
+
+module.exports = function () {process.stdout.write("prompt >");
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  const commands = {
+    pwd: process.cwd(),
+  };
+  process.stdout.write(`answer: ${commands[cmd]}`);
+  process.stdout.write("\nprompt >");
+});
+}
